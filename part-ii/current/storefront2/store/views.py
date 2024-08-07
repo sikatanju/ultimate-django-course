95233adc34fc8015a355070ecf8907c23bfe20cb
sikatanju <saadtanju@gmail.com>
2024-08-07 12:18:57 +0600

    created first api views

--- part-ii/current/storefront2/store/views.py
+++ part-ii/current/storefront2/store/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
+@api_view()
+def product_list(request):
+    return Response("Ok")
+
+@api_view()
+def product_details(request, id):
+    return Response(id)
